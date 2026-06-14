eb225d1db1b26ab4741cc435ac65154a4696568e
up52ashutosh-star <up52ashutosh@gmail.com>
2026-06-14 15:53:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 import streamlit as st
+from transformers import pipeline
 
-st.title("Hello Streamlit!")
-st.write("Deployment successful 🚀")
+@st.cache_resource
+def load_model():
+    return pipeline("sentiment-analysis")
+
+sent_pipeline = load_model()
+
+st.title("Sentiment Analysis App")
+
+review = st.text_area("Enter your review:")
+
+if st.button("Analyze"):
+    if review:
+        result = sent_pipeline(review)[0]
+
+        st.write("Sentiment:", result["label"])
+        st.write("Confidence:", f"{result['score']*100:.2f}%")
