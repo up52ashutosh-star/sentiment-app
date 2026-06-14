c56dfa49e8b00ece6bf794f45225e23620e0f8bc
up52ashutosh-star <up52ashutosh@gmail.com>
2026-06-14 16:29:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
 import streamlit as st
 from transformers import pipeline
 
+# Load model only once (IMPORTANT FOR MEMORY)
 @st.cache_resource
 def load_model():
-    return pipeline(
-        "sentiment-analysis",
-        model="distilbert-base-uncased-finetuned-sst-2-english"
-    )
+    return pipeline("sentiment-analysis")
 
-sent_pipeline = load_model()
+model = load_model()
 
 st.title("Sentiment Analysis App")
 
-review = st.text_area("Enter your review:")
+text = st.text_input("Enter text")
 
 if st.button("Analyze"):
-    if review:
-        result = sent_pipeline(review)[0]
-
-        st.write("Sentiment:", result["label"])
-        st.write("Confidence:", f"{result['score']*100:.2f}%")
+    result = model(text)
+    st.write(result)
